4a6d20022e21bf15ebb80bc17c8d6272c390015f
veeru78866 <veeru78866@gmail.com>
2023-04-28 21:15:30 +0000

    adding new book store using express jsand sqlite

---
+++ nodejs/sessions/Introduction-to-Express-JS-Part-2/myapp/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+//sqlite db module importing
+const { open } = require("sqlite");
+//driver for sqlite
+const sqlite3 = require("sqlite3");
+
+const dbPath = path.join(__dirname, "goodreads.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+
+    app.listen(3000, () => {
+      console.log("Server is Running on http://localhost:3000");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+//getting books list for that we're creating an api
+
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+        SELECT 
+        *
+        FROM
+        book 
+        ORDER BY 
+        book_id;
+    `;
+  const booksList = await db.all(getBooksQuery);
+
+  response.send(booksList);
+});
+
+initializeDBAndServer();

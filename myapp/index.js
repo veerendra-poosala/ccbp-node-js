3ec8360a510090f696e655b5317280c624c1808f
veeru78866 <veeru78866@gmail.com>
2023-08-11 17:56:12 +0000

    updated login api

--- myapp/index.js
+++ myapp/index.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(express.json());
@@ -41,7 +41,6 @@ app.get("/books/", async (request, response) => {
   response.send(booksArray);
 });
 
-
 // User Register API
 app.post("/users/", async (request, response) => {
   const { username, name, password, gender, location } = request.body;
@@ -74,30 +73,25 @@ app.post("/users/", async (request, response) => {
   }
 });
 
-
 // User Login API
-app.post("/login/", async (request, response) => {
+app.post("/login", async (request, response) => {
   const { username, password } = request.body;
-  const selectUserQuery = `
-    SELECT
-      *
-    FROM
-      user
-    WHERE 
-      username = '${username}';`;
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
   const dbUser = await db.get(selectUserQuery);
-
   if (dbUser === undefined) {
     response.status(400);
     response.send("Invalid User");
   } else {
     const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
     if (isPasswordMatched === true) {
-      response.send("Login Success!");
+      const payload = {
+        username: username,
+      };
+      const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+      response.send({ jwtToken });
     } else {
       response.status(400);
       response.send("Invalid Password");
     }
   }
 });
-
